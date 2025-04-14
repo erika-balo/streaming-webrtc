60b62efa30d604960357cee1e25f35c1e20e011c
Jose Manuel <josemanuelgarciadelao@gmail.com>
2025-04-14 16:40:10 -0600

    addd peerjs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const { ExpressPeerServer } = require('peer');  // Importa ExpressPeerServer correctamente
 
 const app = express();
 const server = http.createServer(app);
@@ -11,21 +12,31 @@ const io = socketIo(server, {
   },
 });
 
-
 app.use(cors());
 
-let host_id = null
+// Aquí es donde creas el servidor de PeerJS
+const peerServer = ExpressPeerServer(server, {
+  path: '/myapp',  // El path de tu servidor de señalización
+  secure: true,    // Asegúrate de usar HTTPS si es necesario
+  cors: {
+    origin: '*',  // Permitir solicitudes de cualquier origen
+    methods: ['GET', 'POST'], // Métodos permitidos
+  },
+});
+
+app.use('/peer', peerServer);
+
+let host_id = null;
 io.on('connection', (socket) => {
   console.log('Usuario conectado:', socket.id, host_id);
-  
-  if(host_id){
-    io.emit('in_streaming', {id: host_id});
+
+  if (host_id) {
+    io.emit('in_streaming', { id: host_id });
   }
 
-  // Escuchar cuando el host inicia la transmisión
   socket.on('start-stream', (streamData) => {
     console.log('Transmisión iniciada', streamData);
-    host_id = streamData.peerId
+    host_id = streamData.peerId;
     io.emit('stream-started', streamData);
   });
 
@@ -33,9 +44,8 @@ io.on('connection', (socket) => {
     console.log('Usuario desconectado:', socket.id);
   });
 
-  
   socket.on('stop-streaming', () => {
-    console.log('Transmision detenida:');
+    console.log('Transmisión detenida:');
     host_id = null;
     io.emit('stream-stopped');
   });
@@ -43,7 +53,7 @@ io.on('connection', (socket) => {
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Servidor escuchando en ${PORT}`);
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
 
 app.get('/test', (req, res) => {
